fed54503acedf8b516609cb08d2675e78d6bfb0b
unknown <regh9108@gmail.com>
2024-01-06 22:48:15 -0800

    new

---
+++ py.py
@@ -0,0 +1,30 @@
+
+# pets=['dog', 'cat' , 'Hambester']
+# for x in pets:
+#   print(pets)
+
+
+# name = 'john'
+# pets= ['Dog' , 'cat' , 'Hambster']
+# owner= {'john':'cat','Mike':'dog' }
+
+# if (name == None) :
+#     print ('por ast')
+    
+# if (pets == None) :
+#     print ('por asr')
+    
+# if (owner == None) :
+#     print ('por ast')
+    
+# for key in owner:
+#        print(key, ":", owner[key])
+
+def name():
+ list = [1,2,3,4,5,6]
+ a=0
+ for y in list:
+  a+=y
+ return a / len(list)
+x=name()
+print(x)
